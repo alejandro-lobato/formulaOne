1a9ea7a8563c38190229523c59b936ca9f0f4360
Alejandro Lobato <a.lobato.molina@gmail.com>
2022-04-19 21:40:17 -0500

    Main C# Project containing the F1 classes.

---
+++ FormulaOne.Classes/FormulaOne.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿using FormulaOne.Classes;
+
+namespace FormulaOne.Client;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Season season = new Season();
+    }
+}
